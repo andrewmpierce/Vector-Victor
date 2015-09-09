3cba584e96955ec41a5161e77c6351fef66ae7a4
apierce1992 <apierce1992@gmail.com>
2015-09-09 13:10:47 -0400

    6 tests

--- linear_algebra.py
+++ linear_algebra.py
@@ -13,4 +13,8 @@ def vector_add(vector1, vector2):
     addition = [x + y for x, y in zip(vector1, vector2)]
     return addition
 
-def vector
+def vector_sub(vector1, vector2):
+    if len(vector1) != len(vector2):
+        raise ShapeException
+    subtraction = [x - y for x, y in zip(vector1, vector2)]
+    return subtraction
