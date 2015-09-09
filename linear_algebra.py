83b8df0cd716c877debf5dd78e441f4a9bfbd089
apierce1992 <apierce1992@gmail.com>
2015-09-09 13:07:49 -0400

    Two 3 tests passing

---
+++ linear_algebra.py
@@ -0,0 +1,16 @@
+
+class ShapeException(Exception):
+    pass
+
+
+def shape(vector):
+    num_rows = len(vector)
+    return (num_rows,)
+
+def vector_add(vector1, vector2):
+    if len(vector1) != len(vector2):
+        raise ShapeException
+    addition = [x + y for x, y in zip(vector1, vector2)]
+    return addition
+
+def vector
