621f4f6a7df391c60d1599458f6adc5704087ed4
apierce1992 <apierce1992@gmail.com>
2015-09-09 13:59:43 -0400

    8 Tests passing

--- linear_algebra.py
+++ linear_algebra.py
@@ -18,3 +18,18 @@ def vector_sub(vector1, vector2):
         raise ShapeException
     subtraction = [x - y for x, y in zip(vector1, vector2)]
     return subtraction
+
+# def vector_sum(*args):
+#     length = len(args)
+#     for arg in args:
+#         if len(arg) != length:
+#             raise ShapeException
+#     sum_all = zip(args)
+#     #vector_sum = [sum(row) for row in sum_all]
+#     print(sum_all)
+
+def dot(vector1, vector2):
+    if len(vector1) != len(vector2):
+        raise ShapeException
+    multiply = [x*y for x,y in zip(vector1, vector2)]
+    return sum(multiply)
