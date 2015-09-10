216b6b559b1ebbdda638030aae26ca92072d64f5
apierce1992 <apierce1992@gmail.com>
2015-09-10 10:16:57 -0400

    Fixed some pep8 stuff

--- linear_algebra.py
+++ linear_algebra.py
@@ -7,19 +7,23 @@ def shape(vector):
     num_rows = len(vector)
     return (num_rows,)
 
+
 def raise_except(vector1, vector2):
     if len(vector1) != len(vector2):
         raise ShapeException
+
 
 def vector_add(vector1, vector2):
     raise_except(vector1, vector2)
     addition = [x + y for x, y in zip(vector1, vector2)]
     return addition
 
+
 def vector_sub(vector1, vector2):
     raise_except(vector1, vector2)
     subtraction = [x - y for x, y in zip(vector1, vector2)]
     return subtraction
+
 
 def vector_sum(*args):
     length = len(args[0])
@@ -32,11 +36,11 @@ def vector_sum(*args):
     sum_all = [sum(x) for x in zip(*args)]
     return sum_all
 
+
 def dot(vector1, vector2):
     raise_except(vector1, vector2)
     multiply = [x*y for x,y in zip(vector1, vector2)]
     return sum(multiply)
-
 
 
 def vector_multiply(vector, scalar):
@@ -49,6 +53,7 @@ def vector_mean(*args):
     vector = vector_sum(*args)
     mean = [x/num for x in vector]
     return mean
+
 
 def magnitude(vector):
     exp = [x**2 for x in vector]
